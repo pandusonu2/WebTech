a3235057c6899b87358e05caff2c6ab21631f788
pandusonu2 <pandu.sonu@yahoo.com>
2017-01-25 10:46:36 +0530

    Lab 2

--- check.php
+++ check.php
@@ -1,14 +1,22 @@
 <?php
 	$user=_GET['user'];
 	$pass=_GET['pass'];
+	$type=_GET['type']
 	mysqli=new mysqli("localhost","root","pass","users");
-	$query="SELECT * FROM userdata";
-    if($re=$mysqli->query($query)){
-        while($row=$re->fetch_row()){
-            if($user==$row[0]&&$pass==$row[1]){
-            	echo "true";
-            }
-        }
-    }
-    echo "false";
+	if($type==1){
+		$query="SELECT * FROM userdata";
+	    if($re=$mysqli->query($query)){
+	        while($row=$re->fetch_row()){
+	            if($user==$row[0]&&$pass==$row[1]){
+	            	echo "true";
+	            }
+	        }
+	    }
+	    echo "false";
+	}else{
+		$query="INSERT into userdata(user,password) values()"
+		if($re=$mysqli->query($query)){
+			echo "true";
+		}
+	}
 ?>
